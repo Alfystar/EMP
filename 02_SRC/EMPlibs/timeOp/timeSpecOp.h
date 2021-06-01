fa687acb9650b8f3881a30954a4fd0f04b722e07
Alfystar <alfystar1701@gmail.com>
2021-06-01 14:10:32 +0200

    UART debugged and tested on Linux-Arduino-Stm32

--- 02_SRC/EMPlibs/timeOp/timeSpecOp.h
+++ 02_SRC/EMPlibs/timeOp/timeSpecOp.h
@@ -15,10 +15,10 @@
 
 #define timeSpecClear(tv) (tv.tv_sec = (tv.tv_nsec = 0)
 
-#define timeSpecCmp(a, b, CMP)                                                                                        \
+#define timeSpecCmp(a, b, CMP)                                                                                         \
   ((a.tv_sec == (b.tv_sec) ? (a.tv_nsec CMP(b.tv_nsec) : (a.tv_sec CMP(b.tv_sec))
 
-#define timeSpecAdd(a, b, result)                                                                                     \
+#define timeSpecAdd(a, b, result)                                                                                      \
   do {                                                                                                                 \
     result.tv_sec = a.tv_sec + b.tv_sec;                                                                               \
     result.tv_nsec = a.tv_nsec + b.tv_nsec;                                                                            \
@@ -28,7 +28,7 @@
     }                                                                                                                  \
   } while (0)
 
-#define timeSpecSub(a, b, result)                                                                                     \
+#define timeSpecSub(a, b, result)                                                                                      \
   do {                                                                                                                 \
     result.tv_sec = a.tv_sec - b.tv_sec;                                                                               \
     result.tv_nsec = a.tv_nsec - b.tv_nsec;                                                                            \
@@ -38,7 +38,7 @@
     }                                                                                                                  \
   } while (0)
 
-#define timeSpecSet(ts, sec, nanoSec)                                                                                \
+#define timeSpecSet(ts, sec, nanoSec)                                                                                  \
   do {                                                                                                                 \
     ts.tv_sec = (long)sec + (long)nanoSec / (1000UL * 1000UL * 1000UL);                                                \
     ts.tv_nsec = (long)nanoSec % (1000UL * 1000UL * 1000UL);                                                           \
@@ -46,7 +46,7 @@
 
 #define timeSpecPrint(ts, name)                                                                                        \
   std::cout << name << ".tv_sec=" << ts.tv_sec << "\t" << name << ".tv_nsec=" << ts.tv_nsec << " ("                    \
-            << ts.tv_sec*1000UL + (ts.tv_nsec + (500UL * 1000UL)) / (1000UL * 1000UL) << "ms)\n"
+            << to_string(ts.tv_sec * 1000UL + ((double)ts.tv_nsec / (1000UL * 1000UL))) << "ms)\n"
 ///###################################################################################################
 
 #endif // PIDCTRLDEMO_TIMESPECOP_H
